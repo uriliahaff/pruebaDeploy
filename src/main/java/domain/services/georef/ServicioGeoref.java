aa0a95031f414ff8274a98555a6ace655e14cba9
uriliahaff <liahaffuriel@gmail.com>
2023-06-01 15:25:11 -0300

    apiGeoRef

---
+++ src/main/java/domain/services/georef/ServicioGeoref.java
@@ -0,0 +1,43 @@
+package domain.services.georef;
+
+import domain.services.georef.entities.ListadoDeMunicipios;
+import domain.services.georef.entities.ListadoDeProvincias;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.io.IOException;
+
+public class ServicioGeoref {
+    private static ServicioGeoref instacia = null;
+    private static final String urlAPI = "https://apis.datos.gob.ar/georef/api/";
+    private Retrofit retrofit;
+    private ServicioGeoref(){
+        this.retrofit = new Retrofit.Builder()
+                .baseUrl(urlAPI)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+    public static ServicioGeoref getInstancia(){
+        if(instacia==null){
+            instacia = new ServicioGeoref();
+        }
+        return instacia;
+    }
+
+    public ListadoDeProvincias listadoDeProvincias() throws IOException {
+        GeorefService georefService = this.retrofit.create(GeorefService.class);
+        Call<ListadoDeProvincias> requestListadoProvinciasArg = georefService.provincias();
+        Response<ListadoDeProvincias> responseListadoProvinciasArg = requestListadoProvinciasArg.execute();
+        return responseListadoProvinciasArg.body();
+    }
+
+    public ListadoDeMunicipios listadoDeMunicipiosDeProvincia(int id) throws IOException {
+        GeorefService georefService = this.retrofit.create(GeorefService.class);
+        Call<ListadoDeMunicipios> requestListadoMunicipiosDeProvincia = georefService.municipios(id);
+        Response<ListadoDeMunicipios> responseListadoMunicipiosDeProvincia = requestListadoMunicipiosDeProvincia.execute();
+        return responseListadoMunicipiosDeProvincia.body();
+    }
+
+}
