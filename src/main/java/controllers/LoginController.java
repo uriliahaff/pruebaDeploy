5232b133508a554eb5a7ddf22cd8a07368cc088b
uriliahaff <liahaffuriel@gmail.com>
2023-10-16 22:42:50 -0300

    MVC 0.71

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,13 +1,12 @@
 package controllers;
 
 import domain.Repositorios.RepositorioUsuario;
+import domain.Usuarios.Rol;
 import domain.Usuarios.Usuario;
 import io.javalin.http.Context;
+import io.javalin.security.RouteRole;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class LoginController {
 
@@ -28,6 +27,10 @@ public class LoginController {
         Map<String, Object> model = new HashMap<>();
         System.out.println("Username: " + context.attribute("username"));
 
+        RepositorioUsuario repoUsuario = new RepositorioUsuario();
+        Usuario user = repoUsuario.findUsuarioById(Integer.parseInt(context.cookie("id")));
+
+        model.put("itemsNav", itemsNav(user.getRoles()));
         model.put("username", context.cookie("username"));
 
         context.render("dashboard.hbs", model);
@@ -65,6 +68,49 @@ public class LoginController {
         context.cookie("username","",0);
 
         context.redirect("/login");
+    }
+
+    public String itemsNav(List<Rol> roles){
+        StringBuilder navItems= new StringBuilder();
+       if(contiene(roles, new Rol("admin",null))){
+           navItems.append("            <hr class=\"sidebar-divider\">\n" +
+                           "\n" +
+                           "            <!-- Heading -->\n" +
+                           "            <div class=\"sidebar-heading\">\n" +
+                           "                Administrador\n" +
+                           "            </div>" +
+                           "<li class=\"nav-item\">\n" +
+                           "                <a class=\"nav-link\" href=\"/usuarios\">\n" +
+                           "                    <i class=\"fas fa-fw fa-users\"></i>\n" +
+                           "                    <span>Usuarios</span></a>\n" +
+                           "            </li>")
+                   .append("<li class=\"nav-item\">\n")
+                   .append("    <a class=\"nav-link\" href=\"/cargaEntidades\">\n")
+                   .append("        <i class=\"fas fa-fw fa-building\"></i>\n")
+                   .append("        <span>Entidades Prestadoras</span></a>\n")
+                   .append("</li>")
+                    .append("<li class=\"nav-item \">\n" +
+                            "                <a class=\"nav-link\" href=\"/cargaOrganismos\">\n" +
+                            "                    <i class=\"fas fa-fw fa-building\"></i>\n" +
+                            "                    <span>Organismos de Control</span></a>\n" +
+                            "            </li>")
+                .append("<li class=\"nav-item \">\n" +
+                        "                            <a class=\"nav-link\" href=\"/admin/incidentes\">\n" +
+                        "                                <i class=\"fas fa-fw fa-list\"></i>\n" +
+                        "                                <span>Incidentes</span></a>\n" +
+                        "                        </li>");
+        }
+       return navItems.toString();
+    }
+
+    public static boolean contiene(List<Rol> lista1, Rol rol2) {
+        for (Rol rol1 : lista1) {
+                    if(rol1.getNombre().equals(rol2.getNombre())){
+                        return true;
+                    }
+        }
+
+        return false; // No se encontraron nombres de rol iguales
     }
 
 }
